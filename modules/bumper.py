63b0ddb5bbba8c3d1a5c52c4aae8d27e0bdc482c
Miguel <angelom93@web.de>
2022-12-15 17:59:19 +0100

    pin binding fix

--- modules/bumper.py
+++ modules/bumper.py
@@ -7,44 +7,55 @@ from .module import Module
 
 
 class Bumper(Module):
-    def __init__(self, socket: int, robot_brain: rosys.hardware.RobotBrain):
+    def __init__(self, socket: int, robot_brain: rosys.hardware.RobotBrain, *, in_1: int = 0, in_1_p0: bool = False,
+                 in_2: int = 0, in_2_p0: bool = False, in_3: int = 0, in_3_p0: bool = False, in_4: int = 0,
+                 in_4_p0: bool = False) -> None:
         super().__init__(socket, robot_brain)
-        self.in_1 = None
+        self.in_1 = in_1
+        self.in_1_p0 = in_1_p0
         self.in_1_status = False
-        self.in_2 = None
+        self.in_2 = in_2
+        self.in_2_p0 = in_2_p0
         self.in_2_status = False
-        self.in_3 = None
+        self.in_3 = in_3
+        self.in_3_p0 = in_3_p0
         self.in_3_status = False
-        self.in_4 = None
+        self.in_4 = in_4
+        self.in_4_p0 = in_4_p0
         self.in_4_status = False
+        self.log = logging.getLogger('test_brain.bumper')
         with ui.card():
             ui.markdown(f'###### Socket {self.socket}: bumber')
             with ui.row():
                 with ui.column():
                     with ui.row():
-                        ui.number('in_1 pin').bind_value_to(self, 'in_1')
-                        ui.switch('p0', on_change=lambda e: ui.notify('Switched' if e.value else 'Unswitched'))
+                        ui.number('in_1 pin', format='%.f', value=0).bind_value(self, 'in_1')
+                        ui.switch('p0', on_change=lambda e: ui.notify(
+                            'Switched' if e.value else 'Unswitched')).bind_value(self, 'in_1_p0')
                         ui.icon('highlight_off').classes('text-red').bind_visibility_from(self,
                                                                                           'in_1_status', backward=lambda x: not x)
                         ui.icon('check_circle_outline').classes(
                             'text-green').bind_visibility_from(self, 'in_1_status')
                     with ui.row():
-                        ui.number('in_2 pin').bind_value_to(self, 'in_2')
-                        ui.switch('p0', on_change=lambda e: ui.notify('Switched' if e.value else 'Unswitched'))
+                        ui.number('in_2 pin', format='%.f', value=0).bind_value(self, 'in_2')
+                        ui.switch('p0', on_change=lambda e: ui.notify(
+                            'Switched' if e.value else 'Unswitched')).bind_value(self, 'in_2_p0')
                         ui.icon('highlight_off').classes('text-red').bind_visibility_from(self,
                                                                                           'in_2_status', backward=lambda x: not x)
                         ui.icon('check_circle_outline').classes(
                             'text-green').bind_visibility_from(self, 'in_2_status')
                     with ui.row():
-                        ui.number('in_3 pin').bind_value_to(self, 'in_3')
-                        ui.switch('p0', on_change=lambda e: ui.notify('Switched' if e.value else 'Unswitched'))
+                        ui.number('in_3 pin', format='%.f', value=0).bind_value(self, 'in_3')
+                        ui.switch('p0', on_change=lambda e: ui.notify(
+                            'Switched' if e.value else 'Unswitched')).bind_value(self, 'in_3_p0')
                         ui.icon('highlight_off').classes('text-red').bind_visibility_from(self,
                                                                                           'in_3_status', backward=lambda x: not x)
                         ui.icon('check_circle_outline').classes(
                             'text-green').bind_visibility_from(self, 'in_3_status')
                     with ui.row():
-                        ui.number('in_4 pin').bind_value_to(self, 'in_4')
-                        ui.switch('p0', on_change=lambda e: ui.notify('Switched' if e.value else 'Unswitched'))
+                        ui.number('in_4 pin', format='%.f', value=0).bind_value(self, 'in_4')
+                        ui.switch('p0', on_change=lambda e: ui.notify(
+                            'Switched' if e.value else 'Unswitched')).bind_value(self, 'in_4_p0')
                         ui.icon('highlight_off').classes('text-red').bind_visibility_from(self,
                                                                                           'in_4_status', backward=lambda x: not x)
                         ui.icon('check_circle_outline').classes(
--- modules/rs485.py
+++ modules/rs485.py
@@ -7,13 +7,17 @@ from .module import Module
 
 
 class Rs485(Module):
-    def __init__(self, socket: int, robot_brain: rosys.hardware.RobotBrain):
+    def __init__(self, socket: int, robot_brain: rosys.hardware.RobotBrain, *, rx: int = 0, rx_p0: bool = False, tx: int = 0,
+                 tx_p0: bool = False, out_1: int = 0, out_1_p0: bool = False, in_1: int = 0, in_1_p0: bool = False) -> None:
         super().__init__(socket, robot_brain)
-        self.rx = None
-        self.tx = None
-        self.out_1: int = 0
-        self.out_1_p0 = False
-        self.in_1: int = 0
+        self.rx = rx
+        self.rx_p0 = rx_p0
+        self.tx = tx
+        self.tx_p0 = tx_p0
+        self.out_1 = out_1
+        self.out_1_p0 = out_1_p0
+        self.in_1 = in_1
+        self.in_1_p0 = in_1_p0
         self.in_1_status = False
         self.log = logging.getLogger('test_brain.rs485')
 
@@ -21,18 +25,23 @@ class Rs485(Module):
             ui.markdown(f'###### Socket {self.socket}: rs485')
             with ui.row():
                 with ui.column():
-                    ui.number('rx pin', format='%.f', value=0).bind_value_to(self, 'rx')
-                    ui.switch('p0', on_change=lambda e: ui.notify('Switched' if e.value else 'Unswitched'))
-                    ui.number('tx pin', format='%.f', value=0).bind_value_to(self, 'tx')
-                    ui.switch('p0', on_change=lambda e: ui.notify('Switched' if e.value else 'Unswitched'))
                     with ui.row():
-                        ui.number('out_1 pin', format='%.f', value=0).bind_value_to(self, 'out_1')
+                        ui.number('rx pin', format='%.f', value=0).bind_value(self, 'rx')
                         ui.switch('p0', on_change=lambda e: ui.notify(
-                            'Switched' if e.value else 'Unswitched')).bind_value_to(self, 'out_1_p0')
+                            'Switched' if e.value else 'Unswitched')).bind_value(self, 'rx_p0')
+                    with ui.row():
+                        ui.number('tx pin', format='%.f', value=0).bind_value(self, 'tx')
+                        ui.switch('p0', on_change=lambda e: ui.notify(
+                            'Switched' if e.value else 'Unswitched')).bind_value(self, 'tx_p0')
+                    with ui.row():
+                        ui.number('out_1 pin', format='%.f', value=0).bind_value(self, 'out_1')
+                        ui.switch('p0', on_change=lambda e: ui.notify(
+                            'Switched' if e.value else 'Unswitched')).bind_value(self, 'out_1_p0')
                         ui.button('send On', on_click=self.send_out_1)
                     with ui.row():
-                        ui.number('in_1 pin', format='%.f', value=0).bind_value_to(self, 'in_1')
-                        ui.switch('p0', on_change=lambda e: ui.notify('Switched' if e.value else 'Unswitched'))
+                        ui.number('in_1 pin', format='%.f', value=0).bind_value(self, 'in_1')
+                        ui.switch('p0', on_change=lambda e: ui.notify(
+                            'Switched' if e.value else 'Unswitched')).bind_value(self, 'in_1_p0')
                         ui.icon('highlight_off').classes('text-red').bind_visibility_from(self,
                                                                                           'in_1_status', backward=lambda x: not x)
                         ui.icon('check_circle_outline').classes(
