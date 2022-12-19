8daabc11faf99c8d5a82291d9abff51615fb85ce
Miguel <angelom93@web.de>
2022-12-19 12:05:58 +0100

    Get modules through startup.liz

--- hardware/hardware_manager.py
+++ hardware/hardware_manager.py
@@ -1,32 +1,36 @@
 import logging
 
 import rosys
+from nicegui import ui
 
-from modules import Module
+import modules
 
 
 class HardwareManager():
-    def __init__(self, robot_brain: rosys.hardware.RobotBrain, *,
-                 socket_1: Module = None,
-                 socket_2: Module = None,
-                 socket_3: Module = None,
-                 socket_4: Module = None,
-                 socket_5: Module = None,
-                 socket_6: Module = None
-                 ) -> None:
+    def __init__(self, robot_brain: rosys.hardware.RobotBrain) -> None:
 
         self.UPDATED = rosys.event.Event()
         '''the hardware state has been updated'''
 
         self.robot_brain = robot_brain
         self.sockets = {
-            '1': socket_1,
-            '2': socket_2,
-            '3': socket_3,
-            '4': socket_4,
-            '5': socket_5,
-            '6': socket_6,
+            1: 'none',
+            2: 'none',
+            3: 'none',
+            4: 'none',
+            5: 'none',
+            6: 'none',
         }
+        self.modules = [
+            None,
+            None,
+            None,
+            None,
+            None,
+            None
+        ]
+
+        self.sockets_set: bool = False
         self.rdyp_status: bool = False
         self.vdp_status: bool = False
 
@@ -34,8 +38,27 @@ class HardwareManager():
 
         rosys.on_repeat(self.update, 0.01)
 
-    async def update(self) -> None:
+    async def set_sockets(self) -> None:
+        self.log.info(f'my socke names are {self.sockets}')
+        for socket, name in self.sockets.items():
+            if name == 'rs485_v04':
+                self.modules[socket-1] = modules.Rs485V04(socket, self.robot_brain)
+            if name == 'oogoor_v01':
+                self.modules[socket-1] = modules.OogoorV01(socket, self.robot_brain)
+            if name == 'oogiir_v01':
+                self.modules[socket-1] = modules.OogiirV01(socket, self.robot_brain)
+            if name == 'can_v04':
+                self.modules[socket-1] = modules.CanV04(socket, self.robot_brain)
+            if name == 'bumper_v02':
+                self.modules[socket-1] = modules.BumperV02(socket, self.robot_brain)
+            if name == 'none':
+                self.modules[socket-1] = None
+            if name == 'can_v03':
+                self.modules[socket-1] = modules.CanV03(socket, self.robot_brain)
+        self.sockets_set = True
+        self.log.info(f'my modules are {self.modules} ')
 
+    async def update(self) -> None:
         for time, line in await self.robot_brain.read_lines():
             words = line.split()
 
@@ -47,36 +70,65 @@ class HardwareManager():
                 self.rdyp_status = int(words.pop(0)) == 0
                 self.vdp_status = int(words.pop(0)) == 0
 
-                for socket, module in self.sockets.items():
+                for socket, name in self.sockets.items():
+                    self.log.info(f'{socket}: {name}')
                     if words[0] == '"rs485_v04"':
+                        self.log.info('rs485 found')
+                        self.sockets[socket] = 'rs485_v04'
                         words.pop(0)
-                        module.in_1_status = int(words.pop(0)) == 1
+                        if self.sockets_set:
+                            self.modules[socket-1].in_1_status = int(words.pop(0)) == 1
+                        else:
+                            words.pop(0)
 
                     elif words[0] == '"oogoor_v01"':
+                        self.sockets[socket] = 'oogoor_v01'
                         words.pop(0)
 
                     elif words[0] == '"oogiir_v01"':
+                        self.sockets[socket] = 'oogiir_v01'
                         words.pop(0)
-                        module.in_1_status = int(words.pop(0)) == 1
-                        module.in_2_status = int(words.pop(0)) == 1
+                        if self.sockets_set:
+                            self.modules[socket-1].in_1_status = int(words.pop(0)) == 1
+                            self.modules[socket-1].in_2_status = int(words.pop(0)) == 1
+                        else:
+                            words.pop(0)
+                            words.pop(0)
 
                     elif words[0] == '"can_v04"':
+                        self.sockets[socket] = 'can_v04'
                         words.pop(0)
-                        module.in_1_status = int(words.pop(0)) == 1
+                        if self.sockets_set:
+                            self.modules[socket-1].in_1_status = int(words.pop(0)) == 1
+                        else:
+                            words.pop(0)
 
                     elif words[0] == '"bumper_v02"':
+                        self.sockets[socket] = 'bumper_v02'
                         words.pop(0)
-                        module.in_1_status = int(words.pop(0)) == 1
-                        module.in_2_status = int(words.pop(0)) == 1
-                        module.in_3_status = int(words.pop(0)) == 1
-                        module.in_4_status = int(words.pop(0)) == 1
+                        if self.sockets_set:
+                            self.modules[socket-1].in_1_status = int(words.pop(0)) == 1
+                            self.modules[socket-1].in_2_status = int(words.pop(0)) == 1
+                            self.modules[socket-1].in_3_status = int(words.pop(0)) == 1
+                            self.modules[socket-1].in_4_status = int(words.pop(0)) == 1
+                        else:
+                            words.pop(0)
+                            words.pop(0)
+                            words.pop(0)
+                            words.pop(0)
 
                     elif words[0] == '"none"':
+                        self.sockets[socket] = 'none'
                         words.pop(0)
 
                     elif words[0] == '"can_v03"':
+                        self.sockets[socket] = 'can_v03'
                         words.pop(0)
-                        module.in_1_status = int(words.pop(0)) == 1
-                        module.in_2_status = int(words.pop(0)) == 1
+                        if self.sockets_set:
+                            self.modules[socket-1].in_1_status = int(words.pop(0)) == 1
+                            self.modules[socket-1].in_2_status = int(words.pop(0)) == 1
+                        else:
+                            words.pop(0)
+                            words.pop(0)
 
         self.UPDATED.emit()
--- main.py
+++ main.py
@@ -20,73 +20,14 @@ if is_real:
         robot_brain.lizard_firmware.flash_params = ['xavier']
 
 
-tester_brain_modules = {
-    '1': 'rs485_v04',
-    '2': 'oogiir_v01',
-    '3': 'can_v04',
-    '4': 'oogoor_v01',
-    '5': 'none',
-    '6': 'bumper_v02'
-}
-tested_brain_modules = {
-    '1': 'none',
-    '2': 'none',
-    '3': 'none',
-    '4': 'none',
-    '5': 'none',
-    '6': 'none'
-}
-available_modules = ['none', 'rs485_v04', 'bumper_v02', 'can_v04', 'oogiir_v01', 'oogoor_v01', 'can_v03']
-
-
 @ui.page('/', shared=True)
 async def index():
 
-    def update_modules(value, key):
-        tested_brain_modules[key] = value
-        ui.notify(f'Socket {key} changed to {value}')
-
-    def set_modules():
+    async def set_modules():
         module_row.clear()
-        if test_brain_type.value != 'Tester':
-            with module_row:
-                for key, value in tested_brain_modules.items():
-                    if value != 'none':
-                        if value == 'rs485_v04':
-                            rs485_test = modules.Rs485V04(int(key), robot_brain)
-                            hardware_manager.sockets[key] = rs485_test
-                        if value == 'oogiir_v01':
-                            oogiir_test = modules.OogiirV01(int(key), robot_brain)
-                            hardware_manager.sockets[key] = oogiir_test
-                        if value == 'oogoor_v01':
-                            oogoor_test = modules.OogoorV01(int(key), robot_brain)
-                            hardware_manager.sockets[key] = oogoor_test
-                        if value == 'can_v04':
-                            can_test = modules.CanV04(int(key), robot_brain)
-                            hardware_manager.sockets[key] = can_test
-                        if value == 'bumper_v02':
-                            bumper_test = modules.BumperV02(int(key), robot_brain)
-                            hardware_manager.sockets[key] = bumper_test
-                        if value == 'can_v03':
-                            can_v03_test = modules.CanV03(int(key), robot_brain)
-                            hardware_manager.sockets[key] = can_v03_test
-                    else:
-                        hardware_manager.sockets[key] = None
-        else:
-            with module_row:
-                rs485_tester = modules.Rs485V04(1, robot_brain)
-                oogiir_tester = modules.OogiirV01(2, robot_brain)
-                can_tester = modules.CanV04(3, robot_brain)
-                oogoor_tester = modules.OogoorV01(4, robot_brain)
-                bumper_tester = modules.BumperV02(6, robot_brain)
-            hardware_manager.sockets = {
-                '1': rs485_tester,
-                '2': oogiir_tester,
-                '3': can_tester,
-                '4': oogoor_tester,
-                '5': None,
-                '6': bumper_tester
-            }
+        with module_row:
+            await hardware_manager.set_sockets()
+
     # ui
     ui.colors(primary='#6E93D6', secondary='#53B689', accent='#111B1E', positive='#53B689')
 
@@ -113,62 +54,11 @@ async def index():
             with ui.card():
                 with ui.row():
                     with ui.column():
-                        test_brain_type = ui.radio(['Tester', 'To be tested'], value='Tester',
-                                                   on_change=lambda e: ui.notify(e.value)).props('inline')
-                        ui.button('Set Modules', on_click=set_modules)
-
-                    # fix selection for TesteR Brain
-                    with ui.row().bind_visibility_from(test_brain_type, 'value', value='Tester'):
-                        for key, value in tester_brain_modules.items():
-                            with ui.column():
-                                ui.markdown(f'###### Socket {key}')
-                                ui.select(
-                                    [value], label=f'{key}', value=f'{value}',
-                                    on_change=lambda e: ui.notify(e.value)
-                                )
-
-                    # selection for TesteD brain
-                    with ui.row().bind_visibility_from(test_brain_type, 'value', value='To be tested'):
-                        with ui.column():
-                            ui.markdown(f'###### Socket 1')
-                            select_socket1 = ui.select(
-                                available_modules, value=f'none',
-                                on_change=lambda: update_modules(select_socket1.value, '1')
-                            )
-                        with ui.column():
-                            ui.markdown(f'###### Socket 2')
-                            select_socket2 = ui.select(
-                                available_modules, value=f'none',
-                                on_change=lambda: update_modules(select_socket2.value, '2')
-                            )
-                        with ui.column():
-                            ui.markdown(f'###### Socket 3')
-                            select_socket3 = ui.select(
-                                available_modules, value=f'none',
-                                on_change=lambda: update_modules(select_socket3.value, '3')
-                            )
-                        with ui.column():
-                            ui.markdown(f'###### Socket 4')
-                            select_socket4 = ui.select(
-                                available_modules, value=f'none',
-                                on_change=lambda: update_modules(select_socket4.value, '4')
-                            )
-                        with ui.column():
-                            ui.markdown(f'###### Socket 5')
-                            select_socket5 = ui.select(
-                                available_modules, value=f'none',
-                                on_change=lambda: update_modules(select_socket5.value, '5')
-                            )
-                        with ui.column():
-                            ui.markdown(f'###### Socket 6')
-                            select_socket6 = ui.select(
-                                available_modules, value=f'none',
-                                on_change=lambda: update_modules(select_socket6.value, '6')
-                            )
+                        ui.button('Get Modules', on_click=set_modules)
 
         # pin Selection for TesteD Brain
         with ui.row() as module_row:
-            set_modules()
+            ui.label('Get Modules with the button above')
 
         # development tools
         with ui.row().classes('items-stretch justify-items-stretch').style('flex-wrap:nowrap'):
@@ -180,7 +70,7 @@ async def index():
                         with ui.column():
                             robot_brain.communication.debug_ui()
                         with ui.column():
-                            ui.markdown(f'**Hardware Manager of "{test_brain_type.value}"**').classes('col-grow')
+                            ui.markdown(f'**Hardware Manager**').classes('col-grow')
                             robot_status = ui.markdown()
                             ui.button('Reset Lizard', on_click=robot_brain.restart)
                     ui.timer(1, lambda: robot_status.set_content(
