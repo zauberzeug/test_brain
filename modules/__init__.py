41746bfdb1361e68e847a38cdcac3402fa4ff382
Miguel <angelom93@web.de>
2023-10-04 10:23:13 +0200

    added startup rb27

--- modules/__init__.py
+++ modules/__init__.py
@@ -5,6 +5,7 @@ from .module import Module
 from .oiio import Oiio
 from .oogiir_v05 import OogiirV05
 from .oogiir_v06 import OogiirV06
+from .oogoir_v02 import OogoirV02
 from .oogoor_v01 import OogoorV01
 from .rs485_v03 import Rs485V03
 from .rs485_v04 import Rs485V04
