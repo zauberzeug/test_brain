a43398c75ba693137018fc9b46950cb9aae10bc5
Johannes-Thiel <jojo.thadfun@gmail.com>
2024-11-27 17:41:14 +0100

    add rb28

--- hardware/robot_brains.py
+++ hardware/robot_brains.py
@@ -16,6 +16,14 @@ class RobotBrains:
             ]),
             'rb13': TestBrain(robot_brain, [], []),
             'rb14': TestBrain(robot_brain, [], []),
+            'rb19': TestBrain(robot_brain, ['nand'], [
+                Rs485V05(robot_brain=robot_brain,socket=1,pin1=26,pin2=27,pin3=15,pin3_on_exander=True,pin4=13,pin4_on_exander=True),
+                OogiirV07(robot_brain=robot_brain,socket=2,pin1=5,pin2=4,pin3=36,pin4=13),
+                CanV06(robot_brain=robot_brain,socket=3,pin1=32,pin2=33,pin3=2,pin3_on_exander=True,pin4=14,pin4_on_exander=True),
+                OogoorV01(robot_brain=robot_brain,socket=4,pin1=33,pin1_on_exander=True,pin2=4,pin2_on_exander=True,pin3=32,pin3_on_exander=True,pin4=5,pin4_on_exander=True),
+                IigiirV02(robot_brain=robot_brain,socket=5,pin1=35,pin1_on_exander=True,pin2=18,pin2_on_exander=True,pin3=21,pin3_on_exander=True,pin4=19,pin4_on_exander=True),
+                CanV04(robot_brain=robot_brain,socket=6,pin1=22,pin1_on_exander=True,pin2=23,pin2_on_exander=True,pin3=12,pin3_on_exander=True,pin4=25,pin4_on_exander=True)
+            ]),
             'rb28': TestBrain(robot_brain, ['nand','orin','v05'], [
                 Rs485V04(robot_brain=robot_brain,socket=1,pin1=26,pin2=27,pin3=15,pin3_on_exander=True,pin4=13,pin4_on_exander=True),
                 OogiirV06(robot_brain=robot_brain,socket=2,pin1=5,pin2=4,pin3=36,pin4=13),
@@ -24,6 +32,7 @@ class RobotBrains:
                 IigiirV01(robot_brain=robot_brain,socket=5,pin1=35,pin1_on_exander=True,pin2=18,pin2_on_exander=True,pin3=21,pin3_on_exander=True,pin4=19,pin4_on_exander=True),
                 BumperV02(robot_brain=robot_brain,socket=6,pin1=12,pin1_on_exander=True,pin2=25,pin2_on_exander=True,pin3=22,pin3_on_exander=True,pin4=23,pin4_on_exander=True)
             ]),
+            
         }
         self.current_brain_name = 'placeholder'
         self.current_brain = self._switch_brain('placeholder')
